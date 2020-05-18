db0be00c2a3ee8d46df7bc5ac18c8b11f60e31a4
ziro <palembani@gmail.com>
2020-05-18 13:56:42 +0700

    + Tweaking rateUSD script

--- .scripts/rateUSD.sh
+++ .scripts/rateUSD.sh
@@ -1,4 +1,4 @@
 # get rate value from cnbcindonesia.com
-curl -s "https://www.cnbcindonesia.com/market-data/currencies/IDR=/USD-IDR" | grep -A1 'class="mark_val"' | cut -d'"' -f2 | cut -d'<' -f2 | cut -d'>' -f2 > /tmp/rateUSD.txt
+[ ! -f /tmp/rateUSD.txt ] && curl -s "https://www.cnbcindonesia.com/market-data/currencies/IDR=/USD-IDR" | grep -A1 'class="mark_val"' | cut -d'"' -f2 | cut -d'<' -f2 | cut -d'>' -f2 > /tmp/rateUSD.txt
 # if rateUSD.txt exist then display it while replace mark_val with Kurs USD -> IDR:
 [ -f /tmp/rateUSD.txt ] && sed 's/mark_val/Kurs USD -> IDR:/' /tmp/rateUSD.txt; echo "Sumber: CNBC Indonesia"
