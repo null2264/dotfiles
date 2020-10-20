d6b049972b0e525a262198b670879b097e767dba
ziro <palembani@gmail.com>
2020-10-20 12:09:08 +0700

    + Added TwiddleCase

--- .scripts/resolution.sh
+++ .scripts/resolution.sh
@@ -5,8 +5,9 @@ choices="1366x768
 
 chosen=$(echo -e "$choices" | rofi -dmenu -i -theme ~/.config/rofi/theme/sidebar)
 
+[ -z "$chosen" ] && exit 0
+
 case "$chosen" in
-	1366x768) xrandr --output VGA-1 --mode $chosen && notify-send "Resolution has been changed to" "$chosen";;
-	1360x765) xrandr --output VGA-1 --mode 1360x765_60.00 && notify-send "Resolution has been changed to" "1360x765";;
-	1280x720) xrandr --output VGA-1 --mode $chosen && notify-send "Resolution has been changed to" "$chosen";;
+    1360x765) xrandr --output VGA1 --mode 1360x765_60.00 && notify-send "Resolution has been changed to" "1360x765";;
+    *) xrandr --output VGA1 --mode $chosen && notify-send "Resolution has been changed to" "$chosen";;
 esac
