05ab2b0a51def594c23ec114d3ba53340ec010a0
Ahmad Ansori Palembani <palembani@gmail.com>
2024-04-18 12:24:30 +0700

    chore(nvim): Fully migrated mappings)

--- .config/nvim/lua/null/remap.lua
+++ .config/nvim/lua/null/remap.lua
@@ -18,4 +18,22 @@ map("v", "<C-/>", "gc", { remap = true })
 map("i", "<C-/>", "<esc>gcc", { remap = true })
 
 -- Normal command shortcut for VISUAL mode
-map("v", ".", ":normal .<CR>")
+map("v", ".", ":normal .<cr>")
+
+-- Clear search
+map("n", "<leader>c", "<cmd>noh<cr>")
+
+-- Navigate splits
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-l>", "<C-w>l")
+
+-- Ctrl+P or Ctrl+N alternatives
+-- map("i", "<expr><Up>", vim.fn.pumvisible() == 1 and "<C-n>" or "<Up>")
+-- map("i", "<expr><Down>", vim.fn.pumvisible() == 1 and "<C-p>" or "<Down>")
+-- map("i", "<expr><Tab>", vim.fn.pumvisible() == 1 and "<C-n>" or "<Tab>")
+-- map("i", "<expr><S-Tab>", vim.fn.pumvisible() == 1 and "<C-p>" or "<S-Tab>")
+
+-- Enter to complete
+-- map("i", "<expr><cr>", vim.fn.pumvisible() == 1 and "<C-y>" or "<cr>")
