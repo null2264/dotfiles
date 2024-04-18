5537634904910f1b5b6eee4613094ef201b5633c
Ahmad Ansori Palembani <palembani@gmail.com>
2024-04-18 12:22:40 +0700

    chore(nvim): Fully migrated vim_uni

--- .config/nvim/lua/null/config.lua
+++ .config/nvim/lua/null/config.lua
@@ -1,10 +1,17 @@
-require("null.remap")
-
 local o = vim.opt
 local g = vim.g
 
+o.encoding = "utf-8"
+o.hls = true
 o.nu = true
 o.relativenumber = true
+o.ignorecase = true
+o.smartcase = true
+o.mouse = "a"
+o.splitbelow = true
+o.splitright = true
 
 g.mapleader = " "
 g.guifont = { "Sarasa UI J", ":h16" }
+
+require("null.remap")
