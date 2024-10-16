7f4d3a0260e36592b6054486c33367d0cde78a84
Ahmad Ansori Palembani <palembani@gmail.com>
2024-10-16 08:39:32 +0700

    chore(nix): Put output at the top

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -1,25 +1,6 @@
 {
   description = "null2264's Nix Setup";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
-
-    nix-darwin = {
-      url = "github:LnL7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    system-manager = {
-      url = "github:numtide/system-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
   outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, system-manager }:
   let
     vars = {
@@ -42,5 +23,24 @@
         inherit inputs nixpkgs system-manager home-manager vars;
       }
     );
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    system-manager = {
+      url = "github:numtide/system-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 }
