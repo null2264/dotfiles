b06175839df2ee506feac785d6e0df4796e44017
Ahmad Ansori Palembani <palembani@gmail.com>
2024-10-26 08:58:22 +0700

    chore(nix/floorp): Default settings

--- .config/nix/modules/home-manager/floorp.nix
+++ .config/nix/modules/home-manager/floorp.nix
@@ -7,6 +7,15 @@
     package = null;  # we only want the config
     profiles.${vars.name} = {
       userChrome = builtins.readFile ../../files/floorp/userChrome.css;
+      settings = {
+        "browser.toolbars.bookmarks.visibility" = "newtab";
+        "floorp.browser.sidebar.is.displayed" = false;
+        "floorp.browser.tabbar.settings" = 2;
+        "floorp.browser.tabs.verticaltab" = true;
+        "floorp.browser.tabs.verticaltab.width" = 208;
+        "floorp.verticaltab.hover.enabled" = true;
+        "floorp.tabbar.style" = 2;
+      };
     };
   };
 }
